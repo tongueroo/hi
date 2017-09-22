01a7d47e31ff322b2be70cfdf8281abfd2d0c5cc
Tung Nguyen <tongueroo@gmail.com>
2017-09-21 22:14:40 -0700

    add migration for ecs migration tutorial

--- ufo/task_definitions.rb
+++ ufo/task_definitions.rb
@@ -5,14 +5,20 @@
 # * helper.full_image_name - Docker image name with the tag when docker image is built by ufo. This is defined in ufo/settings.yml.  The helper.full_image_name includes the git sha tongueroo/hi:ufo-[sha].
 # * helper.dockerfile_port - Expose port in the Dockerfile.  Only supports one exposed port, the first one that is encountered.
 #
-# env_vars - is a helper method that generates the proper environment Array of Hashes
-
+# helper.env_vars - is a helper method that generates the proper environment Array of Hashes
+#
+# More info: http://ufoships.com/docs/helpers/
+#
 task_definition "hi-web" do
   source "main" # will use ufo/templates/main.json.erb
   variables(
     family: task_definition_name,
     name: "web",
     container_port: helper.dockerfile_port,
+    # uncomment out to set the log group
+    awslogs_group: "hi-web",
+    awslogs_stream_prefix: "hi",
+    awslogs_region: "us-west-2",
     command: ["bin/web"]
   )
 end
@@ -22,6 +28,10 @@ task_definition "hi-worker" do
   variables(
     family: task_definition_name,
     name: "worker",
+    # uncomment out to set the log group
+    awslogs_group: "hi-worker",
+    awslogs_stream_prefix: "hi",
+    awslogs_region: "us-west-2",
     command: ["bin/worker"]
   )
 end
@@ -31,6 +41,10 @@ task_definition "hi-clock" do
   variables(
     family: task_definition_name,
     name: "clock",
+    # uncomment out to set the log group
+    awslogs_group: "hi-clock",
+    awslogs_stream_prefix: "hi",
+    awslogs_region: "us-west-2",
     command: ["bin/clock"]
   )
 end
--- ufo/variables/base.rb
+++ ufo/variables/base.rb
@@ -1,3 +1,5 @@
+# Example ufo/variables/base.rb
+# More info on how variables work: http://ufoships.com/docs/variables/
 @image = helper.full_image_name # includes the git sha tongueroo/hi:ufo-[sha].
 @cpu = 128
 @memory_reservation = 256
